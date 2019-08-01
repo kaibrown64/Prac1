ed30c73d3659df13c12f272400223c712e34436e
kaibrown644@gmail.com <kaibrown64@gmail.com>
2019-08-01 20:33:21 +0100

    Changed LED0 from pin 7 to pin 11. Added LED1 and LED2 on pins 13 and 15, also alternating once per second. Initialized SW0 and SW1 for input with internal pull-ups. Moved all GPIO initializations to init_GPIO().

--- template.py
+++ template.py
@@ -12,16 +12,28 @@ Date: 30/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
+from itertools import product
+
+# Global Variables
+
 
 # Logic that you write
 def main():
     print("Executing code...press any key to cancel")
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(7, GPIO.OUT)       #set pin 7 as Out for LED
-    while True:	                  #infinite loop
-        GPIO.output(7, not GPIO.input(7)) #sets the value of Pin 7 (GPIO 4) high
-        time.sleep(1)          #wait for 1? second
+    init_GPIO()
+    while True:	                              #infinite loop
+        GPIO.output(11, not (GPIO.input(11))) #invert LED Output
+        GPIO.output(13, not (GPIO.input(13))) #invert LED Output
+        GPIO.output(15, not (GPIO.input(15))) #invert LED Output
+        time.sleep(1)                         #wait for 1 second
 
+def init_GPIO():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)       #set pin 11 as Out for LED1
+    GPIO.setup(13, GPIO.OUT)       #set pin 13 as Out for LED2
+    GPIO.setup(15, GPIO.OUT)       #set pin 15 as Out for LED3
+    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #set pin 16 as In for SW0 with pull-up
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #set pin 18 as In for SW1 with pull-up
 
 # Only run the functions if 
 if __name__ == "__main__":
